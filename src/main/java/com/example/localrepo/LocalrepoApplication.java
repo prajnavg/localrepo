f3888443d6c6be5cee3d36d7cd1a37e4086d90e5
prajnavg <prajnavg@gmail.com>
2025-04-09 10:59:23 +0530

    First commit

---
+++ src/main/java/com/example/localrepo/LocalrepoApplication.java
@@ -0,0 +1,14 @@
+package com.example.localrepo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalrepoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocalrepoApplication.class, args);
+		System.out.println("Welcome to spring.. First code is here!!");
+	}
+
+}
---
+++ src/test/java/com/example/localrepo/LocalrepoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.localrepo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalrepoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
